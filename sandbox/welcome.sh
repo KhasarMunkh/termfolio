15789c5a84a1bb02f42032b199160568f1ce2757
KhasarMunkh <02khasar@gmail.com>
2025-11-28 16:25:46 -0700

    maya easter egg

--- sandbox/welcome.sh
+++ sandbox/welcome.sh
@@ -7,6 +7,7 @@ BLUE='\033[38;2;138;173;244m'
 TEAL='\033[38;2;139;213;202m'
 GREEN='\033[38;2;166;218;149m'
 YELLOW='\033[38;2;238;212;159m'
+PINK='\033[38;2;245;189;230m'
 TEXT='\033[38;2;202;211;245m'
 SUBTEXT='\033[38;2;165;173;203m'
 RESET='\033[0m'
@@ -24,6 +25,7 @@ echo -e "${SUBTEXT}    about     ${TEXT}- Learn about me${RESET}"
 echo -e "${SUBTEXT}    projects  ${TEXT}- Browse my work${RESET}"
 echo -e "${SUBTEXT}    skills    ${TEXT}- Technical skills${RESET}"
 echo -e "${SUBTEXT}    contact   ${TEXT}- Get in touch${RESET}"
+echo -e "${SUBTEXT}    ${PINK}maya${RESET}      ${TEXT}- ???${RESET}"
 echo ""
 echo -e "${YELLOW}  Try:${RESET} ${TEXT}cd ~/projects && nvim .${RESET}"
 echo ""
